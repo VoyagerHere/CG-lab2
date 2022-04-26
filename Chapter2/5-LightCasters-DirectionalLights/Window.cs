74caa31bd616d976796a0569ff4bada9fc4b0e6f
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-04-26 20:16:41 +0300

    upd

--- Chapter2/5-LightCasters-DirectionalLights/Window.cs
+++ Chapter2/5-LightCasters-DirectionalLights/Window.cs
@@ -13,9 +13,6 @@ namespace Lab2
 
         private List<RenderObjects> RenderObj = new List<RenderObjects>();
 
-        private double _timeLimit = 100;
-        private double _time = 0;
-
         private readonly Vector3 _lightPos = new Vector3(1.2f, 1.0f, 2.0f);
 
         private int _vertexBufferObject;
@@ -28,12 +25,10 @@ namespace Lab2
 
         private Shader _lightingShader;
 
-        /* Textures */
-
         private Texture tree_texture;
         private Texture tree_texture_specular;
 
-
+        private double _timeLimit = 100;
 
         private Camera _camera;
 
@@ -50,8 +45,9 @@ namespace Lab2
         {
             base.OnLoad();
 
-            Cylinder cyl = new Cylinder(0f, -10.0f, 7.3f, 6f, 1f);
-            Sphere sphere = new Sphere(1.3f, 0.0f, -10.0f, -6.5f);
+            Cylinder cyl = new Cylinder(0.0f, 0.0f, 0.0f, 1f, 10f);
+            Sphere sphere = new Sphere(2.0f, -0.0f, -10.0f, -6.5f);
+            Triangle tr1 = new Triangle(2.0f, 2.0f, 2.0f, 2.0f);
 
             _camera = new Camera(Vector3.UnitZ * 3, Size.X / (float)Size.Y);
 
@@ -59,29 +55,13 @@ namespace Lab2
 
             GL.Enable(EnableCap.DepthTest);
 
+
             tree_texture = Texture.LoadFromFile("Resources/container2.png");
-            tree_texture_specular = Texture.LoadFromFile("Resources/container2.png");
+            tree_texture_specular = Texture.LoadFromFile("Resources/container2_specular.png");
 
 
             _lightingShader = new Shader("Shaders/shader.vert", "Shaders/lighting.frag");
             _lampShader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
-            
-            {
-                _vaoModel = GL.GenVertexArray();
-                GL.BindVertexArray(_vaoModel);
-
-                var positionLocation = _lightingShader.GetAttribLocation("aPos");
-                GL.EnableVertexAttribArray(positionLocation);
-                GL.VertexAttribPointer(positionLocation, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 0);
-
-                var normalLocation = _lightingShader.GetAttribLocation("aNormal");
-                GL.EnableVertexAttribArray(normalLocation);
-                GL.VertexAttribPointer(normalLocation, 3, VertexAttribPointerType.Float, false, 8 * sizeof(float), 3 * sizeof(float));
-
-                var texCoordLocation = _lightingShader.GetAttribLocation("aTexCoords");
-                GL.EnableVertexAttribArray(texCoordLocation);
-                GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 8 * sizeof(float), 6 * sizeof(float));
-            }
 
             {
                 _vaoLamp = GL.GenVertexArray();
@@ -95,8 +75,8 @@ namespace Lab2
             }
 
 
-            RenderObj.Add(new RenderObjects(cyl.GetAllTogether(), cyl.GetIndices(), tree_texture, tree_texture_specular, _lampShader, 8));
-            RenderObj.Add(new RenderObjects(sphere.GetAllTogether(), sphere.GetIndices(), tree_texture, tree_texture_specular, _lampShader, 8));
+            RenderObj.Add(new RenderObjects(cyl.GetAllTogether(), cyl.GetIndices(), tree_texture, tree_texture_specular, _lightingShader, 8));
+            RenderObj.Add(new RenderObjects(sphere.GetAllTogether(), sphere.GetIndices(), tree_texture, tree_texture_specular, _lightingShader, 8));
 
 
 
@@ -126,7 +106,7 @@ namespace Lab2
             _lightingShader.SetInt("material.diffuse", 0);
             _lightingShader.SetInt("material.specular", 1);
             _lightingShader.SetVector3("material.specular", new Vector3(0.5f, 0.5f, 0.5f));
-            _lightingShader.SetFloat("material.shininess", 32.0f);
+            _lightingShader.SetFloat("material.shininess", 1000.0f);
 
             // Directional light needs a direction, in this example we just use (-0.2, -1.0, -0.3f) as the lights direction
             _lightingShader.SetVector3("light.direction", new Vector3(-0.2f, -1.0f, -0.3f));
@@ -134,35 +114,8 @@ namespace Lab2
             _lightingShader.SetVector3("light.diffuse", new Vector3(0.5f));
             _lightingShader.SetVector3("light.specular", new Vector3(1.0f));
 
+
             DrawSceneObjects();
-
-/*            // We want to draw all the cubes at their respective positions
-            for (int i = 0; i < _cubePositions.Length; i++)
-            {
-                // Then we translate said matrix by the cube position
-                // The model matrix consists of translations, scaling and/or rotations we'd like to apply to transform all object's vertices it to the global world space
-                Matrix4 model = Matrix4.CreateTranslation(_cubePositions[i]);
-                // We then calculate the angle and rotate the model around an axis
-                float angle = 20.0f * i;
-                model = model * Matrix4.CreateFromAxisAngle(new Vector3(1.0f, 0.3f, 0.5f), angle);
-                // Remember to set the model at last so it can be used by opentk
-                _lightingShader.SetMatrix4("model", model);
-
-                // At last we draw all our cubes
-                GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
-            }*/
-/*
-            GL.BindVertexArray(_vaoModel);
-
-            _lampShader.Use();
-
-            Matrix4 lampMatrix = Matrix4.CreateScale(0.2f);
-            lampMatrix = lampMatrix * Matrix4.CreateTranslation(_lightPos);
-
-            _lampShader.SetMatrix4("model", lampMatrix);
-            _lampShader.SetMatrix4("view", _camera.GetViewMatrix());
-            _lampShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);*/
 
             SwapBuffers();
         }
@@ -252,9 +205,9 @@ namespace Lab2
             Object.Bind();
             model *= RotationMatrixX1;
             Object.ApplyTexture();
-            _lampShader.SetMatrix4("model", model);
-            _lampShader.SetMatrix4("view", _camera.GetViewMatrix());
-            _lampShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
+            _lightingShader.SetMatrix4("model", model);
+            _lightingShader.SetMatrix4("view", _camera.GetViewMatrix());
+            _lightingShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
             Object.Render();
         }
 
@@ -267,9 +220,24 @@ namespace Lab2
             Object.Bind();
             model *= RotationMatrixX1 * RotationMatrixX2;
             Object.ApplyTexture();
-            _lampShader.SetMatrix4("model", model);
-            _lampShader.SetMatrix4("view", _camera.GetViewMatrix());
-            _lampShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
+            _lightingShader.SetMatrix4("model", model);
+            _lightingShader.SetMatrix4("view", _camera.GetViewMatrix());
+            _lightingShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
+            Object.Render();
+        }
+
+        public void DrawTriange(int i)
+        {
+            var Object = RenderObj[1];
+            var RotationMatrixX1 = Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(90));
+            var RotationMatrixX2 = Matrix4.CreateTranslation(0, (float)_timeLimit / 4, 0);
+            var model = Matrix4.Identity;
+            Object.Bind();
+            model *= RotationMatrixX1 * RotationMatrixX2;
+            Object.ApplyTexture();
+            _lightingShader.SetMatrix4("model", model);
+            _lightingShader.SetMatrix4("view", _camera.GetViewMatrix());
+            _lightingShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
             Object.Render();
         }
 
