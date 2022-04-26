d562e1900c9d97195f49ead3a2b5eb062f891cc5
ParkerProbe <kolessnikoff.d.e@gmail.com>
2022-04-26 12:49:14 +0300

    init

---
+++ src/BasicTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+
+
+namespace Lab2
+{
+    sealed class Program : GameWindow
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            using (Window game = new Window(800, 600, "LearnOpenTK"))
+            {
+                //Run takes a double, which is how many frames per second it should strive to reach.
+                //You can leave that out and it'll just update as fast as the hardware will allow it.
+                game.Run(60.0);
+            }
+            // var program = new Program();
+            // program.Run();
+        }
+    }
+}
---
+++ src/BlankWindow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenTK;
+
+namespace BlankWindow
+{
+    sealed class Program : GameWindow // Базовый класс opentk
+    {
+        [STAThread]
+        static void Main()
+        {
+            var program = new Program();
+            program.Run();
+        }
+    }
+}
